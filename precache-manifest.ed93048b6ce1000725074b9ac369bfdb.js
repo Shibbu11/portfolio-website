5d2fa309f155ed5628e28f9beb084eaf800385fa
Shibbu11 <shivpratapsingh643@gmail.com>
2019-07-16 16:16:08 +0530

    Updates

---
+++ precache-manifest.ed93048b6ce1000725074b9ac369bfdb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0e1e731819752b5e1982",
+    "url": "/portfolio-website/static/js/runtime~main.0e1e7318.js"
+  },
+  {
+    "revision": "3cfdc3fcb8c4d824406e",
+    "url": "/portfolio-website/static/js/main.3cfdc3fc.chunk.js"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/portfolio-website/static/js/2.9eee67cf.chunk.js"
+  },
+  {
+    "revision": "3cfdc3fcb8c4d824406e",
+    "url": "/portfolio-website/static/css/main.e75b9d70.chunk.css"
+  },
+  {
+    "revision": "dc8c7430047eb11261433c5fb36d56d5",
+    "url": "/portfolio-website/index.html"
+  }
+];
