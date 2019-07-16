74f9026f3e0677ead909106989685b1c41cf8011
Shibbu11 <shivpratapsingh643@gmail.com>
2019-07-16 15:17:55 +0530

    Initial commit

---
+++ src/components/introduction.jsx
@@ -0,0 +1,46 @@
+import React, { Component } from 'react'
+
+export default class Introduction extends Component {
+  render() {
+    return (
+      <div>
+        <section id="colorlib-hero" className="js-fullheight" data-section="home">
+          <div className="flexslider js-fullheight">
+            <ul className="slides">
+              <li style={{backgroundImage: 'url(images/shiv_image.jpg)'}}>
+                <div className="overlay" />
+                <div className="container-fluid">
+                  <div className="row">
+                    <div className="col-md-6 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text">
+                      <div className="slider-text-inner js-fullheight">
+                        <div className="desc">
+                          <h1>Hello! <br />I'm Shiv</h1>
+                          <p><a className="btn btn-primary btn-learn" href="https://drive.google.com/file/d/13ErkETniZOcFdOyeY4agypTttO8gA83M/view" target="_blank" rel="noopener noreferrer">View CV<i className="icon-download4" /></a></p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </li>
+              <li style={{backgroundImage: 'url(images/shiv_image.jpg)'}}>
+                <div className="overlay" />
+                <div className="container-fluid">
+                  <div className="row">
+                    <div className="col-md-6 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text">
+                      <div className="slider-text-inner">
+                        <div className="desc">
+                          <h1>Under Development...</h1>  
+                          {/* <p><a className="btn btn-primary btn-learn" href="" target="_blank" rel="noopener noreferrer">View Projects <i className="icon-briefcase3" /></a></p> */}
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </li>
+            </ul>
+          </div>
+        </section>
+      </div>
+    )
+  }
+}
---
+++ src/components/timeline.jsx
@@ -0,0 +1,76 @@
+import React, { Component } from 'react'
+
+export default class Timeline extends Component {
+  render() {
+    return (
+      <div>
+        <section className="colorlib-experience" data-section="timeline">
+          <div className="colorlib-narrow-content">
+            <div className="row">
+              <div className="col-md-6 col-md-offset-3 col-md-pull-3 animate-box" data-animate-effect="fadeInLeft">
+                <span className="heading-meta">Experience</span>
+                <h2 className="colorlib-heading animate-box">Work Experience and Education</h2>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-md-12">
+                <div className="timeline-centered">
+                  <article className="timeline-entry animate-box" data-animate-effect="fadeInLeft">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-3">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2>Software Engineer at Tracxn <span>July 2019-present</span></h2>
+                        <p>I am currently working as Software Engineer Fullstack. My work involves developing web apps using Groovy, React, MongoDB and Spring Boot.</p>
+                      </div>
+                    </div>
+                  </article>
+                  <article className="timeline-entry animate-box" data-animate-effect="fadeInTop">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-4">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2>Project Engineer at Wipro<span>July 2018- June 2019</span></h2>
+                        <p>I worked at Wipro for thier client CISCO. My project involved working on developing new feature and improve response time for API's which were querying Elasticsearch for the data. Technologies I worked on were Elasticsearch, AngularJS, Highcharts and Spring MVC.</p>
+                      </div>
+                    </div>
+                  </article>
+                  <article className="timeline-entry animate-box" data-animate-effect="fadeInTop">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-4">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2>Software Development Intern at BlogVault<span>Dec 2017- Feb 2018</span></h2>
+                        <p>I developed a web scraping tool using Ruby on Rails which used to collect data from many sources like Youtube, Quora, Reddit etc.</p>
+                      </div>
+                    </div>
+                  </article>
+                  <article className="timeline-entry animate-box" data-animate-effect="fadeInLeft">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-5">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                      <h2>Undergraduation at NIT Srinagar<span>2014-2018</span></h2>
+                      <p>I have completed my graduation in Information Technology. I have studied the subjects like Data Structures, Algorithms, Opearting Systems, Networking as part of my curriculam. My final year project for graduation was 'Multiclass classification of Music Genres using Machine Learning'.</p>  
+                      </div>
+                    </div>
+                  </article>
+                  <article className="timeline-entry begin animate-box" data-animate-effect="fadeInBottom">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-none">
+                      </div>
+                    </div>
+                  </article>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+    )
+  }
+}
